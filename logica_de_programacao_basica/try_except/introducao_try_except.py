7aff9cf0a9cec06e06f39204ee3a49122d758647
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-07-15 20:33:11 -0300

    commit

---
+++ logica_de_programacao_basica/try_except/introducao_try_except.py
@@ -0,0 +1,13 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+print(123)
+print(456)
+
+
+#numero = input("Vou dobrar o número que você digitar: ")
+#numero_float = float(numero)
+
+#print(f"O dobro de {numero} é {numero_float * 2:.2f}")
