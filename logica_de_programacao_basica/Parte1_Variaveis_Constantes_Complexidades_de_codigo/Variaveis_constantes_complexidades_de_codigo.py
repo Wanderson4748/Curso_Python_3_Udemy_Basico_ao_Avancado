8d14f40a55c8be7e49b471019e7f1b3f52e11163
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-07-18 17:25:33 -0300

    commit

--- logica_de_programacao_basica/Parte1_Variaveis_Constantes_Complexidades_de_codigo/Variaveis_constantes_complexidades_de_codigo.py
+++ logica_de_programacao_basica/Parte1_Variaveis_Constantes_Complexidades_de_codigo/Variaveis_constantes_complexidades_de_codigo.py
@@ -11,12 +11,13 @@ RADAR_2 = 85 # Velocidade máxima do radar 2
 LOCAL_RADAR_2 = 110 # Local onde o radar 1 está
 RADAR_RANGE = 1 # A distância onde o radar pega
 
-if velocidade > RADAR_2:
-    print("Velocidade do carro passou do radar 2")
+vel_carro_pass_radar_2 = velocidade > RADAR_2
+carro_passou_radar_2 = local_carro >= (LOCAL_RADAR_2 - RADAR_RANGE)
+local_carro <= (LOCAL_RADAR_2 + RADAR_RANGE)
+carro_multado_radar_2 = carro_passou_radar_2 and vel_carro_pass_radar_2
 
-if local_carro >= (LOCAL_RADAR_2 - RADAR_RANGE) and \
-    local_carro <= (LOCAL_RADAR_2 + RADAR_RANGE) and \
-        velocidade > RADAR_2:
+if carro_multado_radar_2:
+    print("Velocidade do carro passou do radar 2")
     print("Carro multado em radar 2")
 else:
     print("Você passou dentro da velocidade correta no radar!")
