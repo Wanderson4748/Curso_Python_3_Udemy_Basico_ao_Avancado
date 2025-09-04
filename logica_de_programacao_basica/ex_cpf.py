96f2d630f14d5cb7bd274644a09972618f604cd5
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-09-04 17:08:37 -0300

    Create ex_cpf.py

---
+++ logica_de_programacao_basica/ex_cpf.py
@@ -0,0 +1,17 @@
+"""
+CPF: 620.967.063-60
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começaando de 10
+
+Ex:   620.967.063-60 (62096706360)
+      10  9  8  7  6  5  4  3  2
+*     6   2  0  9  6  7  0  6  3
+      60  18 0  63 36 35 0  18 6
+
+Somar todos os resultados:
+70+36+48+56+12+20+32+27+0 = 
+
+
+
+"""
