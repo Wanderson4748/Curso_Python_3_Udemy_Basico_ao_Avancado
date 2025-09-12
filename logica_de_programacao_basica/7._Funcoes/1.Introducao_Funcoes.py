19a15153c918e188a14546513807069e041ad8b8
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-09-12 13:42:58 -0300

    commit

--- logica_de_programacao_basica/7._Funcoes/1.Introducao_Funcoes.py
+++ logica_de_programacao_basica/7._Funcoes/1.Introducao_Funcoes.py
@@ -1,3 +1,12 @@
+"""
+O def é usado para definir uma função
+
+def nome_da_funcao(parametros):
+    bloco de código
+
+nome_da_funcao(argumentos)
+"""
+
 def multiplo_de(numero, multiplo):
     resultado = numero % multiplo == 0
     print(f'{numero} é múltiplo de {multiplo}?', end=' ')
