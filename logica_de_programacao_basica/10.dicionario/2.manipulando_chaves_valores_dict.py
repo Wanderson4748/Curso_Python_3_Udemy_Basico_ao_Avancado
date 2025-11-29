03ad37203cfa42b34c4f69d49c15fecb1dc28822
Wanderson Sousa Rodrigues <141971045+Wanderson4748@users.noreply.github.com>
2025-11-29 09:54:37 -0300

    commit

--- logica_de_programacao_basica/10.dicionario/2.manipulando_chaves_valores_dict.py
+++ logica_de_programacao_basica/10.dicionario/2.manipulando_chaves_valores_dict.py
@@ -1,11 +1,31 @@
 # Manipulando chaves e valores em dicionários
 
-pessoa = {
-    'nome': 'Wanderson Sousa',
-    'sobrenome': 'Rodrigues',
-    'idade': 20,
-    'altura': 1.7,
-    'endereço': [
-        {'rua': 'Manuel Galdino', 'número': 2710}
-    ],
-}
+# lista = []
+# dict = {}
+
+pessoa = {}
+
+##
+##
+
+chave = 'nome'
+
+pessoa[chave] = 'Luiz Otávio'
+pessoa['sobrenome'] = 'Miranda'
+
+
+print(pessoa[chave])
+
+pessoa[chave] = 'Maria'
+
+del pessoa['sobrenome']
+print(pessoa)
+print(pessoa['nome'])
+
+# print(pessoa.get('sobrenome'))
+if pessoa.get('sobrenome') is None:
+    print('NÃO EXISTE')
+else:
+    print(pessoa['sobrenome'])
+
+# print('ISSO Não vai')
